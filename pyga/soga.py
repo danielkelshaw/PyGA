505b1109a7da364049bb05c477c389c3bf77bfac
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-04-12 19:08:00 +0100

    Completed initial SOGA
    This commit marks the first implementation of a SOGA. This is not a particularly effective implementation as there is nothing driving the population towards the optimum - this comes with the addition of an aspect of elitism or at least using some improved version of selection.
    
    Resolves: #5

--- pyga/soga.py
+++ pyga/soga.py
@@ -27,17 +27,20 @@ class SOGA:
             Current iteration of the optimisation process.
         population : list of Individual
             Current state of the population for the optimisation.
-        best_inidividual : Individual
+        best_individual : Individual
             The individual with the most-optimum fitness.
         """
 
         if len(lb) != len(ub):
             raise ValueError(f'len(lb): {len(lb)} != len(ub): {len(ub)}')
 
+        if not n_individuals % 2 == 0:
+            raise ValueError('Please ensure n_individuals is even.')
+
         self.lb = np.asarray(lb)
         self.ub = np.asarray(ub)
 
-        self.n_indviduals = n_individuals
+        self.n_individuals = n_individuals
         self.n_iterations = n_iterations
 
         self.iteration = 0
@@ -57,7 +60,7 @@ class SOGA:
 
         """Generates the population list of Individuals."""
 
-        for _ in range(self.n_indviduals):
+        for _ in range(self.n_individuals):
             self.population.append(Individual(self.lb, self.ub))
 
     def termination_check(self):
@@ -81,21 +84,74 @@ class SOGA:
             Object to compare fitness with best_individual.
         """
 
-        if individual.fitness < self.best_individual.fitness:
+        if self.best_individual is None:
+            self.best_individual = copy.deepcopy(individual)
+        elif individual.fitness < self.best_individual.fitness:
             self.best_individual = copy.deepcopy(individual)
 
-    # TODO >> These methods can be part of a `utils` file.
     @staticmethod
     def select(population):
-        pass
+
+        """
+        Selects a random individual from the population.
+
+        Parameters
+        ----------
+        population : list of Individuals
+            List from which to randomly select and copy an individual.
+
+        Returns
+        -------
+        Individual
+            Copy of random individual selected from population.
+        """
+
+        return copy.deepcopy(np.random.choice(population))
 
     @staticmethod
-    def crossover(parent_a, parent_b):
-        pass
+    def crossover(ia, ib):
+
+        """
+        Performs crossover for two individuals at a random index.
+
+        Parameters
+        ----------
+        ia : Individual
+            First individual to perform crossover.
+        ib : Individual
+            Second individual to perform crossover.
+
+        Returns
+        -------
+        Individual
+            First individual with modified position.
+        Individual
+            Second individual with modified position.
+        """
+
+        ix = np.random.randint(1, len(ia.position))
+        ia.position[:ix], ia.position[ix:] = ib.position[ix:], ib.position[:ix]
+        return ia, ib
 
     @staticmethod
-    def mutate(self):
-        pass
+    def mutate(individual):
+
+        """
+        Slightly alter the position of the individual.
+
+        Parameters
+        ----------
+        individual : Individual
+            Individual for which to mutate the position.
+
+        Returns
+        -------
+        Individual
+            Individual the the mutated position.
+        """
+
+        individual.position = individual.position * np.random.uniform(0.9, 1.1)
+        return individual
 
     @staticmethod
     def evaluate_fitness(individual, fn):
@@ -131,12 +187,16 @@ class SOGA:
 
             for individual in self.population:
                 self.evaluate_fitness(individual, fn)
+                self.update_best(individual)
 
             _population = []
-            for i in range(self.n_indviduals // 2):
-                parent_a, parent_b = self.select(self.population)
+            for i in range(self.n_individuals // 2):
+                parent_a = self.select(self.population)
+                parent_b = self.select(self.population)
                 child_a, child_b = self.crossover(parent_a, parent_b)
                 child_a, child_b = self.mutate(child_a), self.mutate(child_b)
                 _population.extend([child_a, child_b])
+
+            self.population = _population
 
             self.iteration += 1
--- tests/test_soga.py
+++ tests/test_soga.py
@@ -7,23 +7,67 @@ class TestSOGA:
 
     @pytest.fixture
     def soga(self):
-        pass
+
+        lb = [0.0, 0.0]
+        ub = [10.0, 10.0]
+        n_individuals = 10
+        n_iterations = 10
+
+        soga = SOGA(lb, ub, n_individuals, n_iterations)
+        return soga
 
     @pytest.fixture
     def individual(self):
-        pass
 
-    def test_reset_environment(self):
-        pass
+        lb = [0.0, 0.0]
+        ub = [10.0, 10.0]
 
-    def test_initialise_population(self):
-        pass
+        individual = Individual(lb, ub)
+        individual.fitness = 50.0
 
-    def test_termination_check(self):
-        pass
+        return individual
 
-    def test_update_best(self):
-        pass
+    def test_reset_environment(self, soga, individual):
 
-    def test_optimise(self):
-        pass
+        soga.iteration = 5
+        soga.initialise_population()
+        soga.best_individual = individual
+
+        soga.reset_environment()
+        assert soga.iteration == 0
+        assert soga.population == []
+        assert soga.best_individual is None
+
+    def test_initialise_population(self, soga):
+
+        soga.initialise_population()
+
+        assert isinstance(soga.population, list)
+        for individual in soga.population:
+            assert isinstance(individual, Individual)
+
+        assert len(soga.population) == soga.n_individuals
+
+    @pytest.mark.parametrize('it', [5, 10, 15])
+    def test_termination_check(self, soga, it):
+
+        soga.iteration = it
+        ret_bool = soga.termination_check()
+
+        if it == 5:
+            assert not ret_bool
+        elif it == 10:
+            assert not ret_bool
+        elif it == 15:
+            assert ret_bool
+
+    def test_update_best(self, soga, individual):
+
+        lb = [0.0, 0.0]
+        ub = [10.0, 10.0]
+
+        soga.best_individual = Individual(lb, ub)
+        soga.best_individual.fitness = 100.0
+
+        soga.update_best(individual)
+        assert soga.best_individual.fitness == 50.0
