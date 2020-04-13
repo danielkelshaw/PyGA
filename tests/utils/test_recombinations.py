eb50e186b5dd8768860881c169f05e5785001a92
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-04-13 17:12:50 +0100

    Added IntermediateRecombination
    Added an additional recombination method: ```IntermediateRecombination``` - I also developed a ```BaseRecombination``` class which inherits from ```BaseCrossover``` as the functionality of each class in this module is broadly the same and this reduces the boiler plate.
    
    Resolves: #9

--- tests/utils/test_recombinations.py
+++ tests/utils/test_recombinations.py
@@ -13,26 +13,16 @@ def parent_b():
     return Individual([0.0, 0.0, 0.0, 0.0], [50.0, 50.0, 50.0, 50.0])
 
 
-class TestLineRecombination:
+class TestBaseRecombination:
 
-    @pytest.mark.parametrize('p', [-0.25, 0.25])
-    def test_cross(self, parent_a, parent_b, p):
-
-        if p == -0.25:
-            with pytest.raises(ValueError):
-                recombination = LineRecombination(p=p)
-
-        else:
-            recombination = LineRecombination(p=p)
-            ret_a, ret_b = recombination.cross(parent_a, parent_b)
-
-            assert isinstance(ret_a, Individual)
-            assert isinstance(ret_b, Individual)
+    def test_init_raise(self):
+        with pytest.raises(ValueError):
+            recombination = BaseRecombination(p=-0.25)
 
     @pytest.mark.parametrize('pos', [[25.0, 25.0], [75.0, 25.0], [75.0, 75.0]])
     def test__in_bounds(self, pos):
         lb, ub = [0.0, 0.0], [50.0, 50.0]
-        recombination = LineRecombination()
+        recombination = BaseRecombination(p=0.25)
 
         if pos == [25.0, 25.0]:
             assert recombination._in_bounds(pos, lb, ub)
@@ -40,7 +30,23 @@ class TestLineRecombination:
             assert not recombination._in_bounds(pos, lb, ub)
 
 
+class TestLineRecombination:
+
+    def test_cross(self, parent_a, parent_b):
+        recombination = LineRecombination()
+        ret_a, ret_b = recombination.cross(parent_a, parent_b)
+
+        assert isinstance(recombination, (BaseRecombination, BaseCrossover))
+        assert isinstance(ret_a, Individual)
+        assert isinstance(ret_b, Individual)
+
+
 class TestIntermediateRecombination:
 
-    def test_cross(self):
-        pass
+    def test_cross(self, parent_a, parent_b):
+        recombination = LineRecombination()
+        ret_a, ret_b = recombination.cross(parent_a, parent_b)
+
+        assert isinstance(recombination, (BaseRecombination, BaseCrossover))
+        assert isinstance(ret_a, Individual)
+        assert isinstance(ret_b, Individual)
